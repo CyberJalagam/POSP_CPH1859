a167a8ea5555d0bc9a47a03b285cf55ea2e857fe
CyberJalagam <rbinternationalnetwork@gmail.com>
2020-12-18 15:36:46 +0000

    Revert "CPH1859: remove Treble app"
    This reverts commit 06ba8be5883b75201f40dbe88469fa2577a52f6e.

--- device.mk
+++ device.mk
@@ -160,6 +160,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ImsInit
 
+PRODUCT_PACKAGES += \
+    TrebleApp
+
 PRODUCT_COPY_FILES += \
        $(LOCAL_PATH)/permissions/interfaces.xml:system/etc/permissions/interfaces.xml
 
